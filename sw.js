bad4275b998b430648e957854d08cad717eb56a0
shirubamaycon-stack <shirubamaycon@gmail.com>
2026-07-19 12:54:48 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE='escala-v1';
+const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    caches.match(e.request).then(r=>r || fetch(e.request).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(c=>{ if(res.ok) c.put(e.request,copy); });
+      return res;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
+
